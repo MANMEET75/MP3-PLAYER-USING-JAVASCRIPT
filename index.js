f00f69321f90a495470b19614381e102995ed105
MANMEET SINGH (AWM) <97391884+MANMEET75@users.noreply.github.com>
2022-09-26 23:49:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,223 @@
+console.log("Manmeet Singh this side over here");
+
+// let buttonPower=true;
+// let buttonCk=true;
+
+
+let buttonPower=document.getElementById("button1");
+buttonPower.addEventListener("click",buttonPowerHandler);
+
+
+
+
+function buttonPowerHandler() {
+    console.log("I have clicked on the button veere");
+    var audio = new Audio('Power.mp3');
+    audio.play();
+    
+}
+let buttonPause1=document.getElementById("buttonPause1");
+buttonPause1.addEventListener("click",function (element) {
+    console.log("I have clicked on pause over here");
+    location.reload();
+
+
+    
+})
+
+
+let buttonCK=document.getElementById("button2");
+buttonCK.addEventListener("click",buttonCKHandler);
+
+
+
+
+function buttonCKHandler() {
+    console.log("I have clicked on the button veere");
+    var audio = new Audio('Celebrity Killer.mp3');
+    audio.play();
+
+    
+}
+let buttonPause2=document.getElementById("buttonPause2");
+buttonPause2.addEventListener("click",function (element) {
+    console.log("I have clicked on pause over here");
+    location.reload();
+
+
+    
+})
+
+
+let button22=document.getElementById("button3");
+button22.addEventListener("click",button22Handler);
+
+
+
+
+function button22Handler() {
+    console.log("I have clicked on the button veere");
+    var audio = new Audio('22 22 Original.mp3');
+    audio.play();
+
+}
+let buttonPause3=document.getElementById("buttonPause3");
+buttonPause3.addEventListener("click",function (element) {
+    console.log("I have clicked on pause over here");
+    location.reload();
+
+
+    
+})
+
+let buttonself=document.getElementById("button4");
+buttonself.addEventListener("click",buttonselfHandler);
+
+
+
+
+function buttonselfHandler() {
+    console.log("I have clicked on the button veere");
+    var audio = new Audio('Selfmade Chaache Maame.mp3');
+    audio.play();
+    
+}
+let buttonPause4=document.getElementById("buttonPause4");
+buttonPause4.addEventListener("click",function (element) {
+    console.log("I have clicked on pause over here");
+    location.reload();
+
+
+    
+})
+
+
+let buttonlast=document.getElementById("button5");
+buttonlast.addEventListener("click",buttonlastHandler);
+
+
+
+
+function buttonlastHandler() {
+    console.log("I have clicked on the button veere");
+    var audio = new Audio('The Last Ride.mp3');
+    audio.play();
+    
+}
+let buttonPause5=document.getElementById("buttonPause5");
+buttonPause5.addEventListener("click",function (element) {
+    console.log("I have clicked on pause over here");
+    location.reload();
+
+
+    
+})
+let button295=document.getElementById("button6");
+button295.addEventListener("click",button295Handler);
+
+
+
+
+function button295Handler() {
+    console.log("I have clicked on the button veere");
+    var audio = new Audio('295.mp3');
+    audio.play();
+    
+}
+let buttonPause6=document.getElementById("buttonPause6");
+buttonPause6.addEventListener("click",function (element) {
+    console.log("I have clicked on pause over here");
+    location.reload();
+
+
+    
+})
+
+let buttonlegend=document.getElementById("button7");
+buttonlegend.addEventListener("click",buttonlegendHandler);
+
+
+
+
+function buttonlegendHandler() {
+    console.log("I have clicked on the button veere");
+    var audio = new Audio('Legend_1.mp3');
+    audio.play();
+    
+}
+
+let buttonPause7=document.getElementById("buttonPause7");
+buttonPause7.addEventListener("click",function (element) {
+    console.log("I have clicked on pause over here");
+    location.reload();
+
+
+    
+})
+
+let buttonjatt=document.getElementById("button8");
+buttonjatt.addEventListener("click",buttonjattHandler);
+
+
+
+
+function buttonjattHandler() {
+    console.log("I have clicked on the button veere");
+    var audio = new Audio('Jatt Da Muqabla.mp3');
+    audio.play();
+
+    
+}
+let buttonPause8=document.getElementById("buttonPause8");
+buttonPause8.addEventListener("click",function (element) {
+    console.log("I have clicked on pause over here");
+    location.reload();
+
+
+    
+})
+
+let buttonbb=document.getElementById("button9");
+buttonbb.addEventListener("click",buttonbbHandler);
+
+
+
+
+function buttonbbHandler() {
+    console.log("I have clicked on the button veere");
+    var audio = new Audio('Bambiha Bole.mp3');
+    audio.play();
+
+    
+}
+let buttonPause9=document.getElementById("buttonPause9");
+buttonPause9.addEventListener("click",function (element) {
+    console.log("I have clicked on pause over here");
+    location.reload();
+
+
+    
+})
+let buttonfold=document.getElementById("button10");
+buttonfold.addEventListener("click",buttonfoldHandler);
+
+
+
+
+function buttonfoldHandler() {
+    console.log("I have clicked on the button veere");
+    var audio = new Audio('Never_Fold-Sidhu_Moose_Wala-(JattZone.com).mp3');
+    audio.play();
+
+
+    
+}
+let buttonPause10=document.getElementById("buttonPause10");
+buttonPause10.addEventListener("click",function (element) {
+    console.log("I have clicked on pause over here");
+    location.reload();
+
+
+    
+})
+
